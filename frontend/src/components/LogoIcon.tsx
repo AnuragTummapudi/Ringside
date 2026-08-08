a49171e5092fdba94dcb47bfa099dbd9cb5b34d5
Akshay Wesley Kothapalli <akshaywesley@Akshays-MacBook-Air.local>
2026-08-08 16:52:33 +0530

    Update Ringside logo

--- frontend/src/components/LogoIcon.tsx
+++ frontend/src/components/LogoIcon.tsx
@@ -1,12 +1,11 @@
 export default function LogoIcon({ className }: { className?: string }) {
   return (
-    <svg
-      viewBox="0 0 256 256"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <path d="M 128.005 191.173 C 128.448 156.208 156.93 128 192 128 L 192 64 L 128 64 C 128 99.346 99.346 128 64 128 L 64 192 L 128 192 Z M 192 256 L 64 256 C 28.654 256 0 227.346 0 192 L 0 64 L 64 64 L 64 0 L 192 0 C 227.346 0 256 28.654 256 64 L 256 192 L 192 192 Z" />
-    </svg>
+    <span className={`inline-block shrink-0 overflow-hidden ${className}`} aria-hidden="true">
+      <img
+        src="/brand/ringside-logo.png"
+        alt=""
+        className="h-full w-full scale-[1.4] object-contain mix-blend-multiply"
+      />
+    </span>
   )
 }
